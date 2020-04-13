53e89271d6731b9a5b05dba6c1046aba45f41c7e
hu_glb <1045274719@qq.com>
2020-04-13 22:33:32 +0800

    OpenFeign Hystrix

---
+++ cloud-consumer-feign-order80/src/main/java/com/hu/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,24 @@
+package com.hu.springcloud.service;
+
+import com.hu.springcloud.entities.CommonResult;
+import com.hu.springcloud.entities.Payment;
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @ClassName PaymentFeignService
+ * @Description
+ * @Author us
+ * @Date 2020/4/13 19:48
+ * @Version 1.0
+ **/
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id);
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/hu/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,20 @@
+package com.hu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName PaymentHystrixMain8001
+ * @Description
+ * @Author us
+ * @Date 2020/4/13 22:15
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
